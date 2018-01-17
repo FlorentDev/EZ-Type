87adac8394b2641a0e98272a8144f64be9921534
Florent <FlorentDev@users.noreply.github.com>
2018-01-17 17:57:18 +0100

    Add a background during a party

---
+++ moteur/background.c
@@ -0,0 +1,56 @@
+#include <time.h>
+#include <stdlib.h>
+#include "../GfxLib/GfxLib.h"
+
+typedef struct star{
+	int x;
+	int y;
+	struct star *next;
+}STAR;
+
+static STAR *star = NULL;
+
+void background(void){
+	int createStar = rand()%2;
+	if(createStar){
+		if(star==NULL){
+			star = (STAR *) malloc(sizeof(STAR));
+			star->next=NULL;
+			star->x = largeurFenetre();
+			star->y = rand() % hauteurFenetre();
+		}
+		else{
+			STAR *end = star;
+			while(end->next!=NULL){
+				end = end->next;
+			}
+			end->next = (STAR *) malloc(sizeof(STAR));
+			end->next->x = largeurFenetre();
+			end->next->y = rand() % hauteurFenetre();
+			end->next->next = NULL;
+		}
+	}
+	STAR *all = star;
+	while(all!=NULL){
+		couleurCourante(255, 255, 255);
+		rectangle(all->x, all->y, all->x + 1, all->y + 1);
+		all->x = all->x - 10;
+		all = all->next;
+	}
+	if(star!=NULL && star->x<0){
+		STAR *tmp = star;
+		star = star->next;
+		free(tmp);
+	}
+}
+
+void freeBackground(void){
+	STAR *tmp;
+	while(star!=NULL){
+		tmp=star;
+		while(tmp->next!=NULL){
+			tmp = tmp->next;
+		}
+		free(tmp);
+	}
+}
---
+++ moteur/background.h
@@ -0,0 +1,3 @@
+void background(void);
+
+void freeBackground(void);
