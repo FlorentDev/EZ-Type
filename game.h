4627e737e222468586c1b3e294a9d09468d66a2e
David <bi-crosseur@hotmail.fr>
2018-01-17 10:15:57 +0100

    Try to fix core dump on Mac

---
+++ game.h
@@ -0,0 +1,43 @@
+#include "spaceship.h"
+#include "enemy.h"
+#include "bullet.h"
+
+typedef struct {
+	Spaceship spaceship;
+	Enemy *enemies;
+	Bullet *bullets;
+	int score;
+}Game;
+
+
+void startGame(int begin);
+
+int gameState(void);
+
+void gamePause(int pause);
+
+void endGame(void);
+
+Game *gameEvent(void);
+
+Game initGame(void);
+
+Enemy* createEnemy();
+
+Bullet* createBullet(int x, int y, int dir);
+
+Bullet* getLastBullet(Bullet* list);
+
+Enemy* getLastEnemy(Enemy* list);
+
+void insertQueueEnemy(Enemy** list, Enemy* maillon);
+
+void insertQueueBullet(Bullet** list, Bullet* maillon);
+
+void removeBullet(Bullet** list, Bullet** maillon);
+
+void removeEnemy(Enemy** list, Enemy** maillon);
+
+Bullet* bulletBeforeOf(Bullet** list, Bullet* maillon);
+
+Enemy* enemyBeforeOf(Enemy** list, Enemy* maillon);
