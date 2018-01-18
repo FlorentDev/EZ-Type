1505a5a025e67e26d35d04f0351e3d9d6e5756ac
David <bi-crosseur@hotmail.fr>
2018-01-18 10:51:35 +0100

    Add image transparency

--- GfxLib/GfxLib.c
+++ GfxLib/GfxLib.c
@@ -486,7 +486,12 @@ static int *BVR2ARVB(int largeur, int hauteur, const unsigned char *donnees)
 		ptrPixel[0] = ptrDonnees[0];
 		ptrPixel[1] = ptrDonnees[1];
 		ptrPixel[2] = ptrDonnees[2];
-		ptrPixel[3] = 0x0FF;
+		// If the pixel is black, set the alpha to 0
+		if(ptrPixel[0] == 0 && ptrPixel[1] == 0 && ptrPixel[2] == 0) {
+			ptrPixel[3] = 0x000;
+		} else {
+			ptrPixel[3] = 0x0FF;
+		}
 		ptrDonnees += 3; /* On passe a la premiere composante du pixel suivant */
 		ptrPixel += 4; /* On passe au pixel suivant */
 	}
